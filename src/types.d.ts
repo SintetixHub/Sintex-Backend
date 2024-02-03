561f78a42aa8a88bcd51b1fe5605251b63c377ca
lennymercadovaldez <71667982+lennymercadovaldez@users.noreply.github.com>
2024-02-03 11:33:37 -0500

    Añadir importación de Request desde 'express' y definir interfaz Request en 'express-serve-static-core'

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,41 +1,49 @@
+import { Request } from 'express';
+
+declare module 'express-serve-static-core' {
+    interface Request {
+        user?: RequestUser;
+    }
+}
+
 export type ServerResponse = {
-    success: boolean;
-    message: string;
-    data?: any
+        success: boolean;
+        message: string;
+        data?: any
 }
 
 export type Achievements = {
-    projectsDone: number;
-    postsCreated: number;
-    postsLiked: number;
-    leadership: number;
-    colaborator: number;
-    dedication: number;
-    creativity: number;
-    growth: number;
-    compromise: number;
+        projectsDone: number;
+        postsCreated: number;
+        postsLiked: number;
+        leadership: number;
+        colaborator: number;
+        dedication: number;
+        creativity: number;
+        growth: number;
+        compromise: number;
 }
 
 export type User = {
-    _id: string;
-    username: string;
-    email: string;
-    password: string;
-    title: string;
-    description: string;
-    urlAvatar: string;
-    createdAt: string;
-    updatedAt: string;
-    achievements: Achievements;
-    projectsHistory: { name: string, projectId: string }[];
-    postsHistory: { title: string, projectId: string }[];
+        _id: string;
+        username: string;
+        email: string;
+        password: string;
+        title: string;
+        description: string;
+        urlAvatar: string;
+        createdAt: string;
+        updatedAt: string;
+        achievements: Achievements;
+        projectsHistory: { name: string, projectId: string }[];
+        postsHistory: { title: string, projectId: string }[];
 }
 
 export type Post = {
-    _id: string;
-    author: string;
-    content: string;
-    rockets: string[];
-    createdAt: string;
-    updatedAt: string;
+        _id: string;
+        author: string;
+        content: string;
+        rockets: string[];
+        createdAt: string;
+        updatedAt: string;
 }
